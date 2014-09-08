099e61df232c4f5a85f6391241a6a662a6eeeadf
Sergio <asergiop21@hotmail.com>
2014-09-08 00:45:09 -0300

    deployment configs

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/home/deployer/apps/base_generico/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.blog.sock"
+worker_processes 2
+timeout
